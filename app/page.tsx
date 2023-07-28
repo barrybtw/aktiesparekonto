a98a1c93c451e0f0614c8dd3888b7f39f769530d
Nicolai <nicolaibernhardchristensen@gmail.com>
2023-07-28 23:32:50 +0200

    fix again...

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Navigation from '@/components/navigation';
+import Navigation from '@/components/Navigation';
 import Projections from '@/components/projections';
 import Questionnaire from '@/components/questionnaire';
 
